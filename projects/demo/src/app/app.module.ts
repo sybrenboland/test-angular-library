c239c7d21f4357574bf9d2f58df8e8f3932a1b5a
sybrenboland <sybboland@gmail.com>
2019-02-07 10:05:33 +0100

    Added jenkins file

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LibModule } from '../../../lib/src/lib/lib.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
